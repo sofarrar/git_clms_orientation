9b1b96e0abbc15aee3e914188231978c320464ee
scott farrar <sofarrar@gmail.com>
2011-09-27 15:27:44 -0700

    hailing

--- sea_weather.py
+++ sea_weather.py
@@ -1,7 +1,7 @@
 import random
 
 
-msg = ["It's snowing.", "it's raining", "sun-breaks", "chance of showers over the south Sound"]
+msg = ["It's hailing.", "it's raining", "sun-breaks", "chance of showers over the south Sound"]
 print random.choice(msg) 
 
 print "ok"
